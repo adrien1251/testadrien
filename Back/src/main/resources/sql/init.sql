3a54810c44c41216680efda6e90273cf7259d143
ramus <lukas.ramus.etu@univ-lille.fr>
2019-10-16 10:05:52 +0200

    Feat(BDD) : modification de l'insert des utilisateurs avec heritage

--- Back/src/main/resources/sql/init.sql
+++ Back/src/main/resources/sql/init.sql
@@ -1,12 +1,14 @@
 INSERT INTO public.users
-(id, email, first_name, last_name, password, reset_token, role)
-VALUES(1, 'client@comparetout.fr', 'Adrien', 'Deblock', '$2a$12$gPhTV2TjgEreC0L8sjZWqe.dg48cPXdiHTt95GgKei9fcBBO7ZTx2', NULL, 'CUSTOMER') ON CONFLICT DO NOTHING;
+(id,dtype, email, first_name, last_name, password, reset_token, role, birthday, phone_num, sexe)
+VALUES(1, 'CUSTOMER', 'client@comparetout.fr', 'Adrien', 'Deblock', '$2a$12$gPhTV2TjgEreC0L8sjZWqe.dg48cPXdiHTt95GgKei9fcBBO7ZTx2', NULL, 'CUSTOMER','01/01/1959', '1234567890', 'MAN')
+ON CONFLICT DO NOTHING;
+
 INSERT INTO public.users
-(id, email, first_name, last_name, password, reset_token, role)
-VALUES(2, 'fournisseur@comparetout.fr', 'Lukas', 'Ramus', '$2a$12$gPhTV2TjgEreC0L8sjZWqe.dg48cPXdiHTt95GgKei9fcBBO7ZTx2', NULL,'SUPPLIER' ) ON CONFLICT DO NOTHING;
-INSERT INTO public.customer
-(id,birthday, phone_num, sexe, user_id)
-VALUES(1,'01/01/1959', '1234567890', 'MAN', 1 ) ON CONFLICT DO NOTHING;
-INSERT INTO public.supplier
-(id,siret, web_site, user_id)
-VALUES(1,'123456788835345', 'https://google.com', 2 ) ON CONFLICT DO NOTHING;
+(id, dtype, email, first_name, last_name, password, reset_token, role, siret,  web_site)
+VALUES(2, 'SUPPLIER', 'fournisseur@comparetout.fr', 'Lukas', 'Ramus', '$2a$12$gPhTV2TjgEreC0L8sjZWqe.dg48cPXdiHTt95GgKei9fcBBO7ZTx2', NULL,'SUPPLIER', '123456788835345', 'https://google.com' )
+ON CONFLICT DO NOTHING;
+
+INSERT INTO public.users
+(id, dtype, email, first_name, last_name, password, reset_token, role, phone_num)
+VALUES(3, 'ADMIN', 'admin@comparetout.fr', 'Pierre', 'Paul', '$2a$12$gPhTV2TjgEreC0L8sjZWqe.dg48cPXdiHTt95GgKei9fcBBO7ZTx2', NULL,'ADMIN', '1234567844' )
+ON CONFLICT DO NOTHING;
