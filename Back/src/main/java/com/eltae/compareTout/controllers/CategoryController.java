98602c306727d2991ae61174d6b86215c7c761ef
dasilva <elsa.dasilva.etu@univ-lille.fr>
2019-10-23 14:57:31 +0200

    ADD feature export category and criteria for supplier

--- Back/src/main/java/com/eltae/compareTout/controllers/CategoryController.java
+++ Back/src/main/java/com/eltae/compareTout/controllers/CategoryController.java
@@ -7,11 +7,15 @@ import com.eltae.compareTout.services.CategoryService;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+import java.io.File;
 
-import java.io.InputStream;
 
 @RestController
 @RequestMapping(Routes.CATEGORY)
@@ -29,4 +33,23 @@ public class CategoryController extends ExceptionCatcher {
     public ResponseEntity<Integer> createUser(@RequestParam("file") MultipartFile file) {
         return ResponseEntity.status(201).body(this.categoryService.create(file));
     }
-}
+
+    @ApiOperation(value = "Exporter le fichier des catégories")
+    @GetMapping(value="/export-categories",produces="text/csv")
+    public ResponseEntity generateReport() {
+        try {
+            File file = this.categoryService.getCategories();
+
+            return ResponseEntity.ok()
+                    .header("Content-Disposition", "attachment; filename=" + "Categorie_file" + ".csv")
+                    .contentLength(file.length())
+                    .contentType(MediaType.parseMediaType("text/csv"))
+                    .body(new FileSystemResource(file));
+
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to generate report: " + "Categorie_file", ex);
+        }
+    }
+
+
+    }
--- Back/src/main/java/com/eltae/compareTout/services/CategoryService.java
+++ Back/src/main/java/com/eltae/compareTout/services/CategoryService.java
@@ -1,28 +1,25 @@
 package com.eltae.compareTout.services;
-
 import com.eltae.compareTout.entities.Category;
+import com.eltae.compareTout.entities.Criteria;
 import com.eltae.compareTout.exceptions.BadCsvLine;
 import com.eltae.compareTout.repositories.CategoryRepository;
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.transaction.Transactional;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @Transactional
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private File cat_file;
 
     @Autowired
     public CategoryService(CategoryRepository categoryRepository) {
@@ -107,5 +104,104 @@ public class CategoryService {
 //            this.categoryRepository.save(childCategory);
         }
         return this.saveCategorie(records, idx);
+
     }
+
+
+    public File getCategories(){
+
+        List<Category> mother = new ArrayList<>();
+        List<Category> childs = new ArrayList<>();
+        mother = this.categoryRepository.findByParent_idIsNull();
+        CSVWriter writer=  writeDataLineByLine("Categorie_file.csv");
+       for (Category c:mother) {
+        parcoursProfondeur(c,"",writer);
+        }
+        try {
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cat_file;
+
+    }
+    private void parcoursProfondeur(Category g, String pathcate,CSVWriter c) {
+        Iterator<Category> j = g.getChildList().iterator();
+        if(pathcate.length()!=0) {
+            pathcate = pathcate + "/" + g.getName();
+            if(!j.hasNext()) {
+                pathcate=g.getId()+"/"+pathcate;
+                this.writeInCsv(g,pathcate,c);
+            }
+        }
+        else {
+            pathcate = g.getName();
+            if(!j.hasNext()) {
+                pathcate=g.getId()+"/"+pathcate;
+                this.writeInCsv(g,pathcate,c);
+            }
+        }
+        while (j.hasNext()) {
+            Category s = j.next();
+            parcoursProfondeur(s, pathcate,c);
+        }
+    }
+
+
+    private void writeInCsv(Category g, String pathcate,CSVWriter c){
+        List<Criteria> crit=null;
+        crit=g.getCriteriaList();
+        String[] row1=pathcate.split("/");
+            if(row1.length<6){
+            for(int i=row1.length;i<6;i++)
+                pathcate=pathcate+"/";
+        }
+        for(Criteria critete: crit){
+            pathcate=pathcate+"/"+critete.isMandatory();
+            pathcate=pathcate+"/"+critete.getName();
+            pathcate=pathcate+"/"+critete.getUnit();
+            pathcate=pathcate+"/"+critete.getType();
+        }
+
+        String[] row=pathcate.split("/");
+        c.writeNext(row);
+    }
+
+    private  CSVWriter writeDataLineByLine(String filePath) {
+        // first create file object for file placed at location
+        // specified by filepath
+        cat_file = new File(filePath);
+        try {
+            // create FileWriter object with file as parameter
+            FileWriter outputfile = new FileWriter(cat_file);
+
+            // create CSVWriter object filewriter object as parameter
+            CSVWriter writer = new CSVWriter(outputfile, ';',
+                    CSVWriter.NO_QUOTE_CHARACTER,
+                    CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                    CSVWriter.DEFAULT_LINE_END);
+
+            // adding header to csv
+            String[] header = {"Category's last child Id"  ,"Main category", "Child_1 category", "Child_2 category", "Child_3 category", "Child_4 category",
+                                "Is_mandatory_criteria_1","Criteria_1","Unit_1","Type_1", "Is_mandatory_criteria_2","Criteria_2","Unit_2","Type_2", "Is_mandatory_criteria_3","Criteria_3","Unit_3","Type_3",
+                                "Is_mandatory_criteria_4","Criteria_4","Unit_4","Type_4","Is_mandatory_criteria_5","Criteria_5","Unit_5","Type_5", "Is_mandatory_criteria_6","Criteria_6","Unit_6","Type_6",
+                                "Is_mandatory_criteria_7","Criteria_7","Unit_7","Type_7", "Is_mandatory_criteria_8","Criteria_8","Unit_8","Type_8",
+                                "Is_mandatory_criteria_9","Criteria_9","Unit_9","Type_9","Is_mandatory_criteria_10","Criteria_10","Unit_10","Type_10" };
+
+            writer.writeNext(header);
+            return writer;
+        }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        return null;
+    }
+
+
+
+
+
+
+
+
 }
