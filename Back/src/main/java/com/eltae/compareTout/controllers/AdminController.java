ae6848bed6641fc20c5a200b67ec7255c06e8000
ramus <lukas.ramus.etu@univ-lille.fr>
2019-11-22 16:02:27 +0100

    Feat : create and update admin

--- Back/src/main/java/com/eltae/compareTout/controllers/AdminController.java
+++ Back/src/main/java/com/eltae/compareTout/controllers/AdminController.java
@@ -1,17 +1,19 @@
 package com.eltae.compareTout.controllers;
 
 import com.eltae.compareTout.constants.Routes;
-import com.eltae.compareTout.dto.AdminDto;
-import com.eltae.compareTout.dto.UserDto;
+import com.eltae.compareTout.dto.admin.AdminDto;
 import com.eltae.compareTout.exceptionHandler.ExceptionCatcher;
+import com.eltae.compareTout.exceptions.ApplicationException;
 import com.eltae.compareTout.services.AdminService;
-import com.eltae.compareTout.services.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(Routes.ADMIN)
