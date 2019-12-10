f995b27292e85ab08b9214368e2f86bd0af5c3fb
ramus <lukas.ramus.etu@univ-lille.fr>
2019-12-10 23:06:47 +0100

    Test : add test on CategoryService

--- Back/src/test/java/com/eltae/compareTout/services/CategoryServiceTest.java
+++ Back/src/test/java/com/eltae/compareTout/services/CategoryServiceTest.java
@@ -3,10 +3,13 @@ package com.eltae.compareTout.services;
 import com.eltae.compareTout.converter.CategoryConverter;
 import com.eltae.compareTout.dto.category.CategoryDto;
 import com.eltae.compareTout.entities.Category;
+import com.eltae.compareTout.entities.CategoryCriteria;
+import com.eltae.compareTout.exceptions.ApplicationException;
 import com.eltae.compareTout.repositories.CategoryRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
@@ -15,6 +18,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.util.Optional;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -27,35 +31,24 @@ public class CategoryServiceTest {
     @Autowired
     CategoryConverter categoryConverter;
 
+
     @Autowired
     private CategoryRepository categoryRepository;
 
 
     @Test
     public void testCreateCategory() throws IOException {
-        /*
+
         //Entry
-        Long categoryIdEntry = 100L;
-
-        CategoryDto categoryEntry = CategoryDto.builder().build();
-
-        //Effective
-
-        //Mocks
-
-        //Call
         String fileName = "final_categories.csv";
-
         File file = new File(getClass().getClassLoader().getResource("fichiers_CSV/final_categories.csv").getFile());
         InputStream inputStream = new FileInputStream(file);
         MultipartFile multipartFile = new MockMultipartFile(fileName,inputStream);
 
         this.categoryService.create(multipartFile);
         //Asset
-        */
-        Assert.assertEquals(1,1);
-    }
 
+    }
 
 
 
