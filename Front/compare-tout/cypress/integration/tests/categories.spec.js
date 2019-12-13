1ffddf67ccc40ab2593e0cb083137c9d5000a017
elise-lapauw <lapauwelise@gmail.com>
2019-12-13 22:16:18 +0100

    ADD tests on criteria and comparison of products TODO tests on login/subscribe

--- Front/compare-tout/cypress/integration/tests/categories.spec.js
+++ Front/compare-tout/cypress/integration/tests/categories.spec.js
@@ -1,5 +1,9 @@
-import { setRoutes } from "./routes.spec";
-import { assertPlatform } from "@angular/core";
+import {
+  setRoutes
+} from "./routes.spec";
+import {
+  assertPlatform
+} from "@angular/core";
 
 describe('Categories Scenarii', function () {
   beforeEach(function () {
@@ -40,38 +44,14 @@ describe('Categories Scenarii', function () {
       .should('eq', 200);
     cy.get('.result-title')
       .contains(Cypress.config('defaultNbProducts'));
+    cy.get('.product-content')
+      .first()
+      .click();
+    cy.get('.button-confirm')
+      .first()
+      .click();
   });
 
-  it('default user filters by price and brand in Mobile category', function () {
-    cy.visit('/category/all');
-    cy.wait('@initCategories')
-      .its('status')
-      .should('eq', 200);
-    cy.get(':nth-child(3) > .category-title')
-      .click();
-    cy.wait('@childCategories')
-      .its('status')
-      .should('eq', 200);
-    cy.get('#more')
-      .click();
-    cy.get('.sub-category')
-      .contains('Téléphonie')
-      .click();
-    cy.wait('@childCategories')
-      .its('status')
-      .should('eq', 200);
-    cy.get('.subcategory-list')
-      .contains('Téléphones mobiles')
-      .click();
-    cy.wait('@retrieveProducts')
-      .its('status')
-      .should('eq', 201);
-    cy.wait('@retrieveCriteria')
-      .its('status')
-      .should('eq', 200);
-    cy.get('.result-title')
-      .contains(Cypress.config('defaultNbProducts'));
 
-  });
 
 });
--- Front/compare-tout/cypress/integration/tests/criteria.spec.js
+++ Front/compare-tout/cypress/integration/tests/criteria.spec.js
@@ -1,4 +1,6 @@
-import { setRoutes } from "./routes.spec";
+import {
+  setRoutes
+} from "./routes.spec";
 
 describe('Categories Scenarii', function () {
   beforeEach(function () {
@@ -10,6 +12,47 @@ describe('Categories Scenarii', function () {
 
   });
 
- 
+  it('default user filters by price and brand in Mobile category', function () {
+    cy.visit('/category/all');
+    cy.wait('@initCategories')
+      .its('status')
+      .should('eq', 200);
+    cy.get(':nth-child(3) > .category-title')
+      .click();
+    cy.wait('@childCategories')
+      .its('status')
+      .should('eq', 200);
+    cy.get('#more')
+      .click();
+    cy.get('.sub-category')
+      .contains('Téléphonie')
+      .click();
+    cy.wait('@childCategories')
+      .its('status')
+      .should('eq', 200);
+    cy.get('.subcategory-list')
+      .contains('Téléphones mobiles')
+      .click();
+    cy.wait('@retrieveProducts')
+      .its('status')
+      .should('eq', 201);
+    cy.wait('@retrieveCriteria')
+      .its('status')
+      .should('eq', 200);
+    cy.get('.result-title')
+      .contains(Cypress.config('defaultNbProducts'));
+    cy.get('.show-hide-filters')
+      .click();
+      cy.get('.title')
+      .contains('MARQUE')
+      .first()
+      .click();
+      cy.get('.mat-checkbox-inner-container')
+      .first()
+      .click();
+      cy.get('.result-title')
+      .contains(Cypress.config('resultNbProducts'));
+  });
+
 
 });
